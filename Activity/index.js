5fa329fea2fe41528b42eacc25999e29e5912b1f
Jun Jun Gabion <jbgabion@sscgi.com>
2025-01-07 14:06:55 +0800

    Styling the console of the game

--- Activity/index.js
+++ Activity/index.js
@@ -1,4 +1,4 @@
-// // This class represents a Pokemon, containing its name, type, level, hp, defense, and temporary defense.
+// This class represents a Pokemon, containing its name, type, level, hp, defense, and temporary defense.
 class Pokemon {
   constructor(name, type, level, hp, defense) {
     this.name = name;
@@ -42,13 +42,15 @@ class Pokemon {
     this.hp = Math.min(this.hp, 100);
     this.temporaryDefense = 5;
     console.log(
-      `${this.name} has been healed! Current HP: ${this.hp}, Temporary Defense: ${this.temporaryDefense}.`
+      `💖 ${this.name} has been healed! Current HP: ${this.hp}, Temporary Defense: ${this.temporaryDefense}💖 .`
     );
   }
+
   // Calculation of Damage
   calculateDamage() {
     return this.level * 2;
   }
+
   // Power upd of pokemon
   powerUp() {
     this.level += 1;
@@ -57,20 +59,25 @@ class Pokemon {
       `${this.name} has powered up! Level: ${this.level}, Defense: ${this.defense}.`
     );
   }
+
   // 10% chance for a critical hit
   criticalHit() {
     return Math.random() < 0.1;
   }
+
   // Calculation of critical hit
   calculateCriticalDamage(damage) {
     if (this.criticalHit()) {
-      console.log(`${this.name} lands a critical hit!`);
+      console.log(
+        `%c💥${this.name} lands a critical hit!💥`,
+        "color:red;font-size:15;font-weight:bold"
+      );
       return damage * 2;
     }
     return damage;
   }
 }
-
+// ElectricPokemon subclass, extending the base Pokemon class
 class ElectricPokemon extends Pokemon {
   constructor(name, level, hp, defense) {
     super(name, "Electric", level, hp, defense);
@@ -78,18 +85,23 @@ class ElectricPokemon extends Pokemon {
 
   attack(opponent) {
     console.log(
-      "------------------------------------------------------------------------------------------"
+      "%c----------------------------BATTLE BEGIN----------------------------------",
+      "color: red; font-weight: bold; font-size: 20px; font-style: italic;"
     );
     console.log(
-      `${this.trainer.name}'s:   ${this.name} Attack ${opponent.name}`
+      `%c${this.trainer.name}'s:  ⚔️ ${this.name} Attack ${opponent.name}🛡️`,
+      "color: red; font-weight: bold; font-size: 15px; font-style: italic;"
     );
-    console.log(`${this.name} uses Thunderbolt attack on ${opponent.name}`);
+    console.log(
+      `%c⚡⚡⚡${this.name} uses Thunderbolt attack on ${opponent.name}⚡⚡⚡`,
+      "color:  rgb(108, 108, 7);; font-weight: bold; font-size: 20px;"
+    );
     let damage = this.level * 3;
     damage = this.calculateCriticalDamage(damage);
     opponent.receiveDamage(damage);
   }
 }
-
+// FirePokemon subclass, extending the base Pokemon class
 class FirePokemon extends Pokemon {
   constructor(name, level, hp, defense) {
     super(name, "Fire", level, hp, defense);
@@ -97,18 +109,24 @@ class FirePokemon extends Pokemon {
 
   attack(opponent) {
     console.log(
-      "------------------------------------------------------------------------------------------"
+      "%c----------------------------BATTLE BEGIN----------------------------------",
+      "color: red; font-weight: bold; font-size: 20px; font-style: italic;"
     );
     console.log(
-      `${this.trainer.name}'s:  ${this.name} Attack  ${opponent.name}`
+      `%c${this.trainer.name}'s:  ${this.name} Attack  ${opponent.name}🛡️`,
+      "color: red; font-weight: bold; font-size: 15px; font-style: italic;"
     );
-    console.log(`${this.name} uses Fireball attack on ${opponent.name}`);
+    console.log(
+      `%c🔥💥${this.name} uses Fireball attack on ${opponent.name}🔥💥`,
+      "color:  rgb(108, 108, 7);; font-weight: bold; font-size: 20px;"
+    );
     let damage = this.level * 3;
     damage = this.calculateCriticalDamage(damage);
     opponent.receiveDamage(damage);
   }
 }
 
+// EarthPokemon subclass, extending the base Pokemon class
 class EarthPokemon extends Pokemon {
   constructor(name, level, hp, defense) {
     super(name, "Earth", level, hp, defense);
@@ -116,16 +134,23 @@ class EarthPokemon extends Pokemon {
 
   attack(opponent) {
     console.log(
-      "------------------------------------------------------------------------------------------"
+      "%c----------------------------BATTLE BEGIN----------------------------------",
+      "color: red; font-weight: bold; font-size: 20px; font-style: italic;"
     );
-    console.log(`${this.trainer.name}'s: ${this.name} Attack ${opponent.name}`);
-    console.log(`${this.name} uses Earthquake attack on ${opponent.name}`);
+    console.log(
+      `%c${this.trainer.name}'s: ⚔️ ${this.name} Attack ${opponent.name}🛡️`,
+      "color: red; font-weight: bold; font-size: 15px; font-style: italic;"
+    );
+    console.log(
+      `%c🌋${this.name} uses Earthquake attack on ${opponent.name}🌋`,
+      "color:  rgb(108, 108, 7);; font-weight: bold; font-size: 20px;"
+    );
     let damage = this.level * 3;
     damage = this.calculateCriticalDamage(damage);
     opponent.receiveDamage(damage);
   }
 }
-
+// WindPokemon subclass, extending the base Pokemon class
 class WindPokemon extends Pokemon {
   constructor(name, level, hp, defense) {
     super(name, "Wind", level, hp, defense);
@@ -133,16 +158,23 @@ class WindPokemon extends Pokemon {
 
   attack(opponent) {
     console.log(
-      "------------------------------------------------------------------------------------------"
+      "%c----------------------------BATTLE BEGIN----------------------------------",
+      "color: red; font-weight: bold; font-size: 20px; font-style: italic;"
     );
-    console.log(`${this.trainer.name}'s: ${this.name} Attack ${opponent.name}`);
-    console.log(`${this.name} uses Tornado attack on ${opponent.name}`);
+    console.log(
+      `%c${this.trainer.name}'s: ⚔️ ${this.name} Attack ${opponent.name}🛡️`,
+      "color: red; font-weight: bold; font-size: 15px; font-style: italic;"
+    );
+    console.log(
+      `%c💨${this.name} uses Tornado attack on ${opponent.name}💨`,
+      "color:  rgb(108, 108, 7);; font-weight: bold; font-size: 20px;"
+    );
     let damage = this.level * 3;
     damage = this.calculateCriticalDamage(damage);
     opponent.receiveDamage(damage);
   }
 }
-
+// WaterPokemon subclass, extending the base Pokemon class
 class WaterPokemon extends Pokemon {
   constructor(name, level, hp, defense) {
     super(name, "Water", level, hp, defense);
@@ -150,10 +182,17 @@ class WaterPokemon extends Pokemon {
 
   attack(opponent) {
     console.log(
-      "------------------------------------------------------------------------------------------"
+      "%c----------------------------BATTLE BEGIN----------------------------------",
+      "color: red; font-weight: bold; font-size: 20px; font-style: italic;"
     );
-    console.log(`${this.trainer.name}'s: ${this.name} Attack ${opponent.name}`);
-    console.log(`${this.name} uses Water Blast attack on ${opponent.name}`);
+    console.log(
+      `%c${this.trainer.name}'s: ⚔️ ${this.name} Attack ${opponent.name}🛡️`,
+      "color: red; font-weight: bold; font-size: 15px; font-style: italic;"
+    );
+    console.log(
+      `%c🌊${this.name} uses Water Blast attack on ${opponent.name}🌊`,
+      "color:  rgb(108, 108, 7);; font-weight: bold; font-size: 20px;"
+    );
     let damage = this.level * 3;
     damage = this.calculateCriticalDamage(damage);
     opponent.receiveDamage(damage);
@@ -165,6 +204,7 @@ class Trainer {
   constructor(name) {
     this.name = name;
     this.pokemonList = [];
+    this.hasBattled = false; // Track if trainer has battled
   }
   // Assigning the trainer to the Pokemon
   addPokemon(pokemon) {
@@ -178,7 +218,7 @@ class Trainer {
   }
 }
 
-//Battle Simulation
+// Battle Simulation
 class Battle {
   constructor(trainer1, trainer2) {
     this.trainer1 = trainer1;
@@ -190,13 +230,16 @@ class Battle {
     const selectedTrainer2Pokemon = this.trainer2.selectRandomPokemon();
 
     console.log(
-      `The battle between ${this.trainer1.name} and ${this.trainer2.name} begins!`
+      `%c⚡The battle between ${this.trainer1.name} and ${this.trainer2.name} begins!⚡`,
+      "color: red; font-weight:bold; font-size:20px; font-family: 'Courier New';padding-right: 50px; padding-left: 50px;"
     );
     console.log(
-      `${this.trainer1.name}'s Pokémon: ${selectedTrainer1Pokemon.name}`
+      `%c${this.trainer1.name}'s Pokemon: ${selectedTrainer1Pokemon.name}`,
+      "background: rgb(113, 217, 108); padding-left: 50px; padding-right: 50px; font-size:20px; font-family: 'Courier New; "
     );
     console.log(
-      `${this.trainer2.name}'s Pokémon: ${selectedTrainer2Pokemon.name}`
+      `%c${this.trainer2.name}'s Pokemon: ${selectedTrainer2Pokemon.name}`,
+      "background: rgb(113, 217, 108);  padding-right: 50px; padding-left: 50px; font-size:20px; font-family: 'Courier New;  "
     );
 
     try {
@@ -231,60 +274,67 @@ class Battle {
 
     if (selectedTrainer1Pokemon.hp <= 0) {
       console.log(`${this.trainer1.name} has lost the battle!`);
+      return false; // Trainer 1 lost
     } else if (selectedTrainer2Pokemon.hp <= 0) {
       console.log(`${this.trainer2.name} has lost the battle!`);
+      return true; // Trainer 1 wins
     }
   }
 
+  // Set a tournament with up to 5 rounds
   startTournament() {
-    const trainer1Pokemon = [...this.trainer1.pokemonList];
-    const trainer2Pokemon = [...this.trainer2.pokemonList];
+    let winner = this.trainer1;
+    let rounds = 0;
 
     console.log(
-      `The tournament between ${this.trainer1.name} and ${this.trainer2.name} begins!`
+      `%cThe Tournament begins with %c${this.trainer1.name}`,
+      "color: White; font-size: 25px; font-family: 'Courier New'; font-weight: bold; text-transform: uppercase; background: rgb(87, 79, 79); padding-left:200px",
+      "color: yellow; font-size: 25px; font-family: 'Courier New'; font-weight: bold; text-transform: uppercase; background: rgb(87, 79, 79); padding-right:200px"
     );
 
-    while (trainer1Pokemon.length > 0 && trainer2Pokemon.length > 0) {
-      const pokemon1 = trainer1Pokemon[0];
-      const pokemon2 = trainer2Pokemon[0];
+    while (
+      rounds < 5 &&
+      this.trainer1.pokemonList.length > 0 &&
+      this.trainer2.pokemonList.length > 0
+    ) {
+      console.log(
+        `%c----------------------------ROUND ${
+          rounds + 1
+        }----------------------------------`,
+        "color: red; font-weight: bold; font-size: 20px; font-style: italic;"
+      );
+      console.log(
+        `%cRound ${rounds + 1}: ${winner.name} continues to battle!`,
+        "color: red; font-weight:bold; font-size: 20px; font-style: italic"
+      );
+      const randomTrainer1 = winner;
+      let randomTrainer2;
 
-      console.log(`Battle: ${pokemon1.name} vs ${pokemon2.name}`);
+      do {
+        randomTrainer2 = this.getRandomTrainer();
+      } while (randomTrainer2 === randomTrainer1);
 
-      while (pokemon1.hp > 0 && pokemon2.hp > 0) {
-        if (Math.random() < 0.2) {
-          console.log(`${pokemon1.name} randomly heals!`);
-          pokemon1.heal();
-        } else {
-          pokemon1.attack(pokemon2);
-        }
+      const battle = new Battle(randomTrainer1, randomTrainer2);
+      const result = battle.start();
 
-        if (pokemon2.hp > 0) {
-          if (Math.random() < 0.2) {
-            console.log(`${pokemon2.name} randomly heals!`);
-            pokemon2.heal();
-          } else {
-            pokemon2.attack(pokemon1);
-          }
-        }
+      if (!result) {
+        // If trainer1 loses, remove from tournament
+        winner = randomTrainer2;
       }
 
-      if (pokemon1.hp <= 0) {
-        console.log(`${pokemon1.name} has fainted!`);
-        trainer1Pokemon.shift();
-      }
-
-      if (pokemon2.hp <= 0) {
-        console.log(`${pokemon2.name} has fainted!`);
-        trainer2Pokemon.shift();
-      }
+      rounds++;
     }
 
-    const winner =
-      trainer1Pokemon.length > 0 ? this.trainer1.name : this.trainer2.name;
-    console.log(
-      "------------------------------------------------------------------------------------------"
+    console.log("The tournament has ended!");
+    console.log(`${winner.name} is the champion of the tournament!`);
+  }
+
+  // Get a random trainer who has not lost
+  getRandomTrainer() {
+    const trainers = [ash, brook, ken, carl, gary].filter(
+      (trainer) => trainer !== this.trainer1 && trainer !== this.trainer2
     );
-    console.log(`${winner} wins the tournament!`);
+    return trainers[Math.floor(Math.random() * trainers.length)];
   }
 }
 
